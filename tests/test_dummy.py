689d74c6e12d753acaa46a30c7b2b02a43b2b2a3
lingjie <lingjie@u.nus.edu>
2021-07-16 20:21:40 +0800

    added dummy test

---
+++ tests/test_dummy.py
@@ -0,0 +1,13 @@
+"""Dummy test."""
+import unittest
+
+
+class Mytest(unittest.TestCase):
+
+    def test_dummy(self):
+        """Does nothing but return true."""
+        return True
+
+
+if __name__ == "__main__":
+    unittest.main()
